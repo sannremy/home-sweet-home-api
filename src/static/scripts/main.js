0705fd911010f26b28a73fbc79a64c2a8c391809
Sann-Remy Chea <sannremy.chea@gmail.com>
2017-12-20 07:53:57 +0100

    Added simple ajax call

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,3 +1,27 @@
 ((global) => {
+  let getServiceByName = (serviceName) => {
+    let httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      console.log('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          console.log(JSON.parse(httpRequest.responseText));
+        } else {
+          console.log('There was a problem with the request.');
+        }
+      }
+    };
+
+    httpRequest.open('GET', '/service/' + serviceName);
+    httpRequest.send();
+  };
+
+  getServiceByName('weather');
+  getServiceByName('traffic');
 
 })(window);
