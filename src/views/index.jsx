10747ad949fde24d2de36bc1caf64e1708954f9f
Sann-Remy Chea <sannremy.chea@gmail.com>
2017-12-23 08:28:49 +0100

    Change site layout

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -5,7 +5,27 @@ class HelloMessage extends React.Component {
   render() {
     return (
       <DefaultLayout title={this.props.title}>
-        <div>Hello!</div>
+        <div className="container">
+          <div className="row py-2">
+            <div className="col-sm">
+              Weather
+            </div>
+            <div className="col-sm">
+              Traffic
+            </div>
+          </div>
+          <div className="row py-2">
+            <div className="col-sm">
+              Water level
+            </div>
+            <div className="col-sm">
+              idk
+            </div>
+            <div className="col-sm">
+              Devices
+            </div>
+          </div>
+        </div>
       </DefaultLayout>
     );
   }
--- src/views/layouts/default.jsx
+++ src/views/layouts/default.jsx
@@ -5,19 +5,17 @@ class DefaultLayout extends React.Component {
     return (
       <html className="no-js">
         <head>
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <title>{this.props.title}</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans:300:400,700" rel="stylesheet" />
-            <link rel="stylesheet" href="/static/styles/main.css" />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <title>{this.props.title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300:400,700" rel="stylesheet" />
+          <link rel="stylesheet" href="/static/styles/main.css" />
         </head>
 
         <body>
-            <div className="Site">
-              {this.props.children}
-            </div>
-            <script src="/static/scripts/main.js"></script>
+          {this.props.children}
+          <script src="/static/scripts/main.js"></script>
         </body>
       </html>
     );
