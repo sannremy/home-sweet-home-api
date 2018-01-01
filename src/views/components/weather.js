226e28a44334c58a8cd280c48b3dddc536ddbb24
Sann-Remy Chea <sannremy.chea@gmail.com>
2018-01-01 15:52:19 +0100

    Bind icons to weather code

--- src/views/components/weather.js
+++ src/views/components/weather.js
@@ -119,7 +119,7 @@ class _WeatherForecast extends React.Component {
                   <div>{day.high}</div>
                   <div>{day.low}</div>
                   <div>{day.text}</div>
-                  <i className="wi wi-day-sunny"></i>
+                  <_WeatherIcon code={day.code} />
                 </li>;
       });
     }
@@ -131,5 +131,178 @@ class _WeatherForecast extends React.Component {
         }
       </div>
     );
+  }
+}
+
+class _WeatherIcon extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      code: parseInt(props.code, 10)
+    };
+  }
+
+  render() {
+    // Gist: https://gist.github.com/Kepro/9ea2a918fd6f0a58b474
+    // Icons from https://erikflowers.github.io/weather-icons/
+    let icon = '';
+
+    switch(this.state.code) {
+      case 0:
+        icon = 'tornado';
+        break;
+      case 1:
+        icon = 'storm-showers';
+        break;
+      case 2:
+        icon = 'tornado';
+        break;
+      case 3:
+        icon = 'thunderstorm';
+        break;
+      case 4:
+        icon = 'thunderstorm';
+        break;
+      case 5:
+        icon = 'snow';
+        break;
+      case 6:
+        icon = 'rain-mix';
+        break;
+      case 7:
+        icon = 'rain-mix';
+        break;
+      case 8:
+        icon = 'sprinkle';
+        break;
+      case 9:
+        icon = 'sprinkle';
+        break;
+      case 10:
+        icon = 'hail';
+        break;
+      case 11:
+        icon = 'showers';
+        break;
+      case 12:
+        icon = 'showers';
+        break;
+      case 13:
+        icon = 'snow';
+        break;
+      case 14:
+        icon = 'storm-showers';
+        break;
+      case 15:
+        icon = 'snow';
+        break;
+      case 16:
+        icon = 'snow';
+        break;
+      case 17:
+        icon = 'hail';
+        break;
+      case 18:
+        icon = 'hail';
+        break;
+      case 19:
+        icon = 'cloudy-gusts';
+        break;
+      case 20:
+        icon = 'fog';
+        break;
+      case 21:
+        icon = 'fog';
+        break;
+      case 22:
+        icon = 'fog';
+        break;
+      case 23:
+        icon = 'cloudy-gusts';
+        break;
+      case 24:
+        icon = 'cloudy-windy';
+        break;
+      case 25:
+        icon = 'thermometer';
+        break;
+      case 26:
+        icon = 'cloudy';
+        break;
+      case 27:
+        icon = 'night-cloudy';
+        break;
+      case 28:
+        icon = 'day-cloudy';
+        break;
+      case 29:
+        icon = 'night-cloudy';
+        break;
+      case 30:
+        icon = 'day-cloudy';
+        break;
+      case 31:
+        icon = 'night-clear';
+        break;
+      case 32:
+        icon = 'day-sunny';
+        break;
+      case 33:
+        icon = 'night-clear';
+        break;
+      case 34:
+        icon = 'day-sunny-overcast';
+        break;
+      case 35:
+        icon = 'hail';
+        break;
+      case 36:
+        icon = 'day-sunny';
+        break;
+      case 37:
+        icon = 'thunderstorm';
+        break;
+      case 38:
+        icon = 'thunderstorm';
+        break;
+      case 39:
+        icon = 'thunderstorm';
+        break;
+      case 40:
+        icon = 'storm-showers';
+        break;
+      case 41:
+        icon = 'snow';
+        break;
+      case 42:
+        icon = 'snow';
+        break;
+      case 43:
+        icon = 'snow';
+        break;
+      case 44:
+        icon = 'cloudy';
+        break;
+      case 45:
+        icon = 'lightning';
+        break;
+      case 46:
+        icon = 'snow';
+        break;
+      case 47:
+        icon = 'thunderstorm';
+        break;
+      case 3200:
+        icon = 'cloud';
+        break;
+      default:
+        icon = 'cloud';
+        break;
+    }
+
+    const classNames = ['wi', 'wi-' + icon];
+
+    return <i className={classNames.join(' ')} />;
   }
 }
