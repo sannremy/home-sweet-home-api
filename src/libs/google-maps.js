117ef4ebb7a9295346867cc85c63509b9f10be24
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-09-29 14:01:06 +0200

    Fixed Maps

--- src/libs/google-maps.js
+++ src/libs/google-maps.js
@@ -10,47 +10,40 @@ const googleMapsClient = GoogleMapsApi.createClient({
 
 class GoogleMaps {
   static async getDurations() {
-    let origins = config.googleMaps.directions.map(direction => direction.origin);
-    let destinations = config.googleMaps.directions.map(direction => direction.destination);
-    let labels = config.googleMaps.directions.map(direction => direction.label);
+    let durations = [];
+    for (let direction of config.googleMaps.directions) {
+      let duration = await googleMapsClient.distanceMatrix({
+        origins: [direction.origin],
+        destinations: [direction.destination],
+        departure_time: (new Date()).getTime() + 1000 * 60 * 5,
+        mode: 'driving',
+        avoid: ['tolls']
+      }).asPromise().then((response) => {
+        let directionDuration = response.json.rows[0].elements[0].duration_in_traffic.value; // in second
+        let directionDistance = response.json.rows[0].elements[0].distance.value; // in meter
 
-    const formattedDurations = await googleMapsClient.distanceMatrix({
-      origins: origins,
-      destinations: destinations,
-      mode: 'driving',
-      avoid: ['tolls'],
-      traffic_model: 'best_guess',
-      departure_time: 'now',
-    }).asPromise().then((response) => {
-      const durations = [];
-      const rows = response.json.rows;
-      for (let i = 0; i < rows.length; i++) {
-        const element = rows[i].elements[0];
+        return {
+          label: direction.label,
+          duration: directionDuration,
+          distance: directionDistance
+        };
+      }).catch((err) => {
+        console.error(err);
 
-        durations.push({
-          label: labels[i],
-          duration: element.duration_in_traffic.value, // in second
-          distance: element.distance.value, // in meter
-        });
-      }
+        if (err === 'timeout') {
+          // Handle timeout.
+        } else if (err.json) {
+          // Inspect err.status for more info.
+        } else {
+          // Handle network error.
+        }
+      });
 
-      return durations;
-    }).catch((err) => {
-      console.error(err);
-
-      if (err === 'timeout') {
-        // Handle timeout.
-      } else if (err.json) {
-        // Inspect err.status for more info.
-      } else {
-        // Handle network error.
-      }
-
-      return [];
-    });
+      durations.push(duration);
+    }
 
     return {
-      durations: formattedDurations
+      durations: durations
     };
   }
 }
