f5d2b03aa709366abdc5cf79ae62cd8c2b8e7bb6
Sann-Remy Chea <sannremy.chea@gmail.com>
2020-01-03 17:40:59 +0100

    Fixed Tuya get access token

--- src/libs/tuya-plug.js
+++ src/libs/tuya-plug.js
@@ -1,25 +1,27 @@
-const crypto = require('crypto');
+const hmacSHA256 = require('crypto-js/hmac-sha256');
 const config = require('config');
 const axios = require('axios');
 
 class TuyaPlug {
-  static async getDevices() {
+  static async getToken() {
     const url = 'https://openapi.tuyaeu.com/v1.0/token?grant_type=1';
-    const t = Date.now();
+    const t = new Date().getTime();
 
     const sign_method = 'HMAC-SHA256';
-    const sign = crypto.createHmac('sha256', config.tuya.clientSecret)
-      .update(config.tuya.clientId + t)
-      .digest('base64').toUpperCase();
+
+    const sign = hmacSHA256(config.tuya.clientId + t, config.tuya.clientSecret).toString().toUpperCase();
 
     let response = await axios.get(url, {
       headers: {
+        client_id: config.tuya.clientId,
         sign_method,
         sign,
         t,
       }
     });
-    console.log(url, response.data);
+
+    const data = response.data;
+    return data.result.access_token;
   }
 }
 
